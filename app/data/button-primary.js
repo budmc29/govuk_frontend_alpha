4efbfeb0bb9a42e29ba95fcfd7be0a9c5dd9b779
Gemma Leigh <hello@gemmaleigh.com>
2016-11-11 17:19:30 +0000

    Import a button component

---
+++ app/data/button-primary.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Primary button',
+  context: {
+    'class': 'govuk-c-button govuk-c-button--primary',
+    'text': 'Primary button text',
+    'is-primary': true
+  }
+}
---
+++ app/data/button.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Button',
+  context: {
+    class: 'govuk-c-button',
+    text: 'Default button text',
+    'is-primary': false
+  }
+}
